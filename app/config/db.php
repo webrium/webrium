18d79aea231a988954d9a288237dbe94e1dea3df
سید بنیامین خلیفه <nvioir@gmail.com>
2020-12-21 00:24:43 +0330

    update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -1,17 +1,16 @@
 <?php
-use webrium\mysql\DB;
+use webrium\foxql\DB;
 
-$config=[];
-
-// $config['main']=[
-//   'driver'=>'mysql' ,
-//   'db_host'=>'localhost' ,
-//   'db_host_port'=>3306 ,
-//   'db_name'=>'test' ,
-//   'username'=>'root' ,
-//   'password'=>'1234' ,
-//   'charset'=>'utf8mb4' ,
-//   'result_stdClass'=>true
-// ];
-
-DB::setConfig($config);
+/**
+ * set mysql config
+ */
+DB::addConfig('main',[
+  'driver'=>'mysql' ,
+  'db_host'=>'localhost' ,
+  'db_host_port'=>3306 ,
+  'db_name'=>'test' ,
+  'username'=>'root' ,
+  'password'=>'1234' ,
+  'charset'=>'utf8mb4' ,
+  'result_stdClass'=>true
+]);
