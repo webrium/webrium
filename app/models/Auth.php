60926bad0af01eae2f4d9b1fb44bbc5a7118a650
Benjamin Khalife <31080657+parsgit@users.noreply.github.com>
2022-02-06 00:38:48 +0330

    Update Auth.php

--- app/models/Auth.php
+++ app/models/Auth.php
@@ -7,7 +7,7 @@ class Auth{
 
   public function user($id)
   {
-    return DB::table('users')->cache('user',function ($q)
+    return DB::table('users')->cache('user',function ($q) use($id)
     {
       return $q->find($id);
     });
