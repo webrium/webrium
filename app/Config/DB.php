10a1700a71413e83c0473f9c6bee597c18cfaf5a
or <nvioir@gmail.com>
2023-02-25 14:01:02 +0330

    Add the .env.example file

--- app/Config/DB.php
+++ app/Config/DB.php
@@ -2,14 +2,16 @@
 
 use Foxdb\DB;
 use Foxdb\Config;
+use Webrium\App;
+
 
 DB::addConnection('main', [
     'host'=>'localhost',
     'port'=>'3306',
 
-    'database'=>'test',
-    'username'=>'root',
-    'password'=>'1234',
+    'database'=> App::env('database', 'test'),
+    'username'=> App::env('username', 'root'),
+    'password'=> App::env('password', '1234'),
 
     'charset'=>Config::UTF8MB4,
     'collation'=>Config::UTF8MB4_GENERAL_CI,
