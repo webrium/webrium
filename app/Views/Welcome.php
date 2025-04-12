0f34af0aa44feaf767e83715a8dd4e05234068d1
parsgit <nvioir@gmail.com>
2025-04-12 21:39:47 +0000

    fix: update HTML structure and improve formatting; update package versions in composer.json

--- app/Views/Welcome.php
+++ app/Views/Welcome.php
@@ -1,32 +1,37 @@
 <!doctype html>
 <html class="h-full min-h-full">
-<head >
+
+<head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/css/style.css" rel="stylesheet">
 </head>
+
 <body class="flex bg-gray-800 justify-center">
 
   <div class="pt-10 mt-10 p-10 shadow-md shadow-green-400/50 rounded-xl bg-gray-700">
 
     <div class="t">
-      <div class="w-28 h-28 flex items-center justify-center mx-auto text-center text-gray-700 rounded-full bg-green-500">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20">
+      <div
+        class="w-28 h-28 flex items-center justify-center mx-auto text-center text-gray-700 rounded-full bg-green-500">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-20 h-20">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
         </svg>
       </div>
     </div>
 
     <div class="w-full flex justify-center mt-4">
-       <h1 class="text-3xl font-bold text-white"> {{ $name }} </h1>
+      <h1 class="text-3xl font-bold text-white"> {{ $name }} </h1>
     </div>
 
-     <div class="mt-10 mx-auto flex justify-center">
-       <a target="_blank" href="https://github.com/webrium/webrium" class="hover:bg-gray-800 duration-200 hover:shadow hover:shadow hover:shadow-white/50 text-white px-8 py-2 border-2 border-green-300/50 rounded-xl">Get Start</a>
-     </div>
+    <div class="mt-10 mx-auto flex justify-center">
+      <a target="_blank" href="https://github.com/webrium/webrium"
+        class="hover:bg-gray-800 duration-200 hover:shadow hover:shadow hover:shadow-white/50 text-white px-8 py-2 border-2 border-green-300/50 rounded-xl">Get
+        Start</a>
+    </div>
 
   </div>
-
 
 </body>
 
