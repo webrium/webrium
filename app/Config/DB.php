a0092d89e6157ade13aa205ff6a3c4c7052067d0
benkhalife <nvioir@gmail.com>
2026-06-10 23:32:25 +0330

    chore(db): update DB.php connection syntax for foxdb v4

--- app/Config/DB.php
+++ app/Config/DB.php
@@ -4,15 +4,13 @@ use Foxdb\DB;
 use Foxdb\Config;
 
 
-DB::addConnection('main', [
-    'host'=> env('host', 'localhost'),
-    'port'=> env('port', 3306),
-
-    'database'=> env('database', 'test'),
-    'username'=> env('username', 'root'),
-    'password'=> env('password', '1234'),
-
-    'charset'=>Config::UTF8MB4,
-    'collation'=>Config::UTF8MB4_UNICODE_CI,
-    'fetch'=>Config::FETCH_CLASS
+DB::addConnection([
+    'driver' => Config::MYSQL,
+    'fetch' => Config::FETCH_OBJ,
+    'throw_exceptions' => true,
+    'host' => env('host', 'localhost'),
+    'port' => env('port', 3306),
+    'database' => env('database', 'test'),
+    'username' => env('username', 'root'),
+    'password' => env('password', '1234'),
 ]);
