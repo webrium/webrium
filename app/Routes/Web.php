84f0cb1539c38ebb89a752307129a663c3e4adf8
benkhalife <nvioir@gmail.com>
2026-06-10 23:29:47 +0330

    refactor(routes): migrate to class-based controller syntax

--- app/Routes/Web.php
+++ app/Routes/Web.php
@@ -1,6 +1,13 @@
 <?php
+
+/**
+ * Web Routes
+ *
+ * Register your application's web routes here.
+ * Each route is mapped to a controller action.
+ */
+
 use App\Controllers\IndexController;
 use Webrium\Route;
 
-Route::get('/',[IndexController::class, 'index']);
-
+Route::get('/', [IndexController::class, 'index']);
